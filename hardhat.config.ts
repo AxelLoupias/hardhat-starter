3484c795f106a400a09307bac46e1a46b571a403
Axel Loupias <axelloupias@gmail.com>
2023-06-09 19:57:15 +0200

    Update config linting and optimize hardhat config
    Signed-off-by: Axel Loupias <axelloupias@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import type { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+import "tsconfig-paths/register";
 
 const config: HardhatUserConfig = {
-    solidity: '0.8.18',
+    solidity: {
+        version: '0.8.18',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 100,
+            },
+        },
+    },
+    mocha: {
+        timeout: 1000000,
+    },
 }
 
 export default config
